d7acca2ff7e4bd24cab174458023f0fcf79fd8f5
Lopson Balzhinimaev <lopson@superdao.co>
2023-10-06 10:28:12 +0200

    starting from scratch

--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -1,13 +1,1 @@
-#![deny(missing_docs)]
-//! Implementation of client/server for KV engines – default/sled
 
-pub use client::KvClient;
-pub use engines::{KvEngine, KvStore, SledKvEngine};
-pub use error::{KvError, Result};
-pub use server::KvServer;
-
-mod client;
-mod common;
-mod engines;
-mod error;
-mod server;
